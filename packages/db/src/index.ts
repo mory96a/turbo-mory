d20a798eeaa7bdbd4de39bdc4ce191367a24c8e6
mory96a <aliaskarimorteza1996@gmail.com>
2026-05-05 23:56:07 +0330

    feat: add @repo/auth with better-auth and prisma adapter

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,9 @@
-export * from '@prisma/client';
+import {PrismaPg} from '@prisma/adapter-pg';
+import {env} from 'prisma/config';
+import {PrismaClient} from './generated/client';
+
+const adapter = new PrismaPg({
+  connectionString: env('DATABASE_URL'),
+});
+
+export const prisma = new PrismaClient({adapter});
