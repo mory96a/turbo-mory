9b0cd6e260f67bda754444053cf9494a96bd49c0
mory96a <aliaskarimorteza1996@gmail.com>
2026-05-13 01:23:47 +0330

    feat: add affected log to cd

---
+++ apps/web/tests/e2e/test.spec.ts
@@ -0,0 +1,15 @@
+import {expect, test} from '@playwright/test';
+
+test('has title', async ({page}) => {
+  await page.goto('https://playwright.dev/');
+
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({page}) => {
+  await page.goto('https://playwright.dev/');
+
+  await page.getByRole('link', {name: 'Get started'}).click();
+
+  await expect(page).toHaveURL(/.*intro/);
+});
