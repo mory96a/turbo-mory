ec0ee0354c68897d9f860e3a35507704718512dd
mory96a <aliaskarimorteza1996@gmail.com>
2026-05-12 23:56:11 +0330

    fix: fix wrong import

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,4 +1,4 @@
-import {Button} from '@repo/ui/button';
+import {Button} from '@repo/ui';
 import Image, {type ImageProps} from 'next/image';
 import styles from './page.module.css';
 
@@ -63,9 +63,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="docs" className={styles.secondary}>
-          Open alert
-        </Button>
+        <Button className={styles.secondary}>Open alert</Button>
       </main>
       <footer className={styles.footer}>
         <a
